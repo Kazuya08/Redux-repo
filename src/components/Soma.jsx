89504a4cde089445e81ec62a12c67aa03d5b8f2a
Kazuya08 <kazuyathiago@hotmail.com>
2021-06-17 14:45:01 -0300

    Conectando Componentes com Redux #02

--- src/components/Soma.jsx
+++ src/components/Soma.jsx
@@ -1,17 +1,28 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
+import { connect } from "react-redux";
 
 import Card from "./Card";
 
-export default (props) => {
+function Soma(props) {
+  const { min, max } = props;
   return (
     <Card title="Soma dos Números" blue>
       <div>
         <span>
           <span>Resultado:</span>
-          <strong>{5467}</strong>
+          <strong>{min + max}</strong>
         </span>
       </div>
     </Card>
   );
-};
+}
+
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max,
+  };
+}
+
+export default connect(mapStateToProps)(Soma);
--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -1,17 +1,28 @@
-/* eslint-disable import/no-anonymous-default-export */
 import React from "react";
+import { connect } from "react-redux";
 
 import Card from "./Card";
 
-export default (props) => {
+function Sorteio(props) {
+  const { min, max } = props;
+  const aleatorio = parseInt(Math.random() * (max - min) + min);
   return (
     <Card title="Sorteio de um Números" purple>
       <div>
         <span>
           <span>Resultado:</span>
-          <strong>{23}</strong>
+          <strong>{aleatorio}</strong>
         </span>
       </div>
     </Card>
   );
-};
+}
+
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max,
+  };
+}
+
+export default connect(mapStateToProps)(Sorteio);
