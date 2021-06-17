dc76fc510608ec25400d7596156aa0ca4ef472c0
Kazuya08 <kazuyathiago@hotmail.com>
2021-06-17 17:03:18 -0300

    Invocando Action Creator #02

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -4,24 +4,32 @@ import React from "react";
 import { connect } from "react-redux";
 
 import Card from "./Card";
-import { alterarNumeroMin } from "../store/actions/numeros";
+import { alterarNumeroMin, alterarNumeroMax } from "../store/actions/numeros";
 
 function Intervalo(props) {
   const { min, max } = props;
 
-  props.alterarMin(10000);
+  // props.alterarMin(10000);
 
   return (
     <Card title="Intervalo de Números" red>
       <div className="Intervalo">
         <span>
           <strong>Mínimo:</strong>
-          <input type="number" value={min} />
+          <input
+            type="number"
+            value={min}
+            onChange={(e) => props.alterarMin(+e.target.value)}
+          />
         </span>
 
         <span>
           <strong>Máximo:</strong>
-          <input type="number" value={max} readOnly />
+          <input
+            type="number"
+            value={max}
+            onChange={(e) => props.alterarMax(+e.target.value)}
+          />
         </span>
       </div>
     </Card>
@@ -35,14 +43,18 @@ function mapStateToProps(state) {
   };
 }
 
-function mapActionCreatorsToProps(dispatch) {
+function mapDispatchToProps(dispatch) {
   return {
     alterarMin(novoNumero) {
       // action creator -> action
       const action = alterarNumeroMin(novoNumero);
+      dispatch(action); //ira chamar todos os reducers e o estado ira mudar
+    },
+    alterarMax(novoNumero) {
+      const action = alterarNumeroMax(novoNumero);
       dispatch(action);
     },
   };
 }
 
-export default connect(mapStateToProps, mapActionCreatorsToProps)(Intervalo);
+export default connect(mapStateToProps, mapDispatchToProps)(Intervalo);
--- src/store/actions/numeros.js
+++ src/store/actions/numeros.js
@@ -5,3 +5,10 @@ export function alterarNumeroMin(novoNumero) {
     payload: novoNumero,
   };
 }
+
+export function alterarNumeroMax(novoNumero) {
+  return {
+    type: "NUM_MAX_ALTERADO",
+    payload: novoNumero,
+  };
+}
